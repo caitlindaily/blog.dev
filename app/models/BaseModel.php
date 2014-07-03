89ec9ed2264f6685388100c45e09645e7a8f90ad
sweetrocker22 <cdaily87@gmail.com>
2014-07-03 09:27:48 -0500

    Made accessors to change created/updated at to local time in models

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -2,7 +2,21 @@
 
 use Carbon\Carbon;
 
+
 class BaseModel extends Eloquent {
 
+	public function getCreatedAtAttribute($value) {
+
+		$utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+    	return $utc->setTimezone('America/Chicago');
 
 	}
+
+	public function getUpdatedAtAttribute($value) {
+
+		$utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+    	return $utc->setTimezone('America/Chicago');
+
+	}
+
+}
